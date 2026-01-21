a03d920bff48c7c3b86f258ab1858b51210ae007
Julian Clement <pieti30tv@gmail.com>
2026-01-21 19:28:30 +0100

    added JSX to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Header from "./App/main_page/Header.jsx"
+import Header from "./app/main_page/Header.jsx"
+import Footer from "./App/main_page/Footer.jsx"
+import Main from "./App/main_page/Main.jsx"
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
     
   );
