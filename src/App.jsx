e85cc495508b54f09ede7c50c65eedcac1097cb2
Julian Clement <pieti30tv@gmail.com>
2026-01-21 19:15:23 +0100

    Fixed and finished Header in Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./App/main_page/Header.jsx"
 
 function App() {
+  return (
+    <>
+      <Header />
+    </>
+    
+  );
 }
 
 export default App
---
+++ src/App/main_page/Header.jsx
@@ -0,0 +1,21 @@
+
+function Header(){
+
+    return(
+        
+        <header>
+            <div id="container greeting" class="container header">
+                <h1 id="">
+                    Willkommen auf der Mathe-Website
+                </h1>
+            </div>
+            <div id ="container info" class="container subtitle">
+                <h3>Hier findest du viele nützliche Informationen und Ressourcen rund um das Thema Mathematik.</h3>
+            </div>
+        </header>
+
+    );
+
+}
+
+export default Header
