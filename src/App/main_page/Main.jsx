65f1767e201be37509c7e21e5ff776e2c633d67d
Julian Clement <pieti30tv@gmail.com>
2026-01-21 19:28:12 +0100

    Added Main to Main Page

---
+++ src/App/main_page/Main.jsx
@@ -0,0 +1,17 @@
+
+function Main() {
+
+    return(
+        <main>
+            <div className="Buttons_AW">
+                <a href="#"><button id="tgr rechner link" className="link button mainPage">TGR Rechner</button></a>
+                <a href="#"><button id="kreisflaeche link" className="link button mainPage">Kreisflächen-Berechnung</button></a>
+                <a href="#"><button id="glch rechner link" className="link button mainPage">Dreisatzrechner</button></a>
+                <a href="#"><button id="umrechner link" className="link button mainPage">Maßeinheiten-Umrechnung</button></a>
+            </div>
+        </main>
+    );
+
+}
+
+export default Main
