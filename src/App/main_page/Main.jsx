b3ce53e64f28bf09950c598ddd7366087acd5e2a
Julian Clement <pieti30tv@gmail.com>
2026-01-22 17:46:16 +0100

    fixed className in Main (MPage)

--- src/App/main_page/Main.jsx
+++ src/App/main_page/Main.jsx
@@ -3,7 +3,7 @@ function Main() {
 
     return(
         <main>
-            <div className="Buttons_AW">
+            <div className="container buttons">
                 <a href="#"><button id="tgr rechner link" className="link button mainPage">TGR Rechner</button></a>
                 <a href="#"><button id="kreisflaeche link" className="link button mainPage">Kreisflächen-Berechnung</button></a>
                 <a href="#"><button id="glch rechner link" className="link button mainPage">Dreisatzrechner</button></a>
